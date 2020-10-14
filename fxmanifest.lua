b13cb49a2ff02aa50326e51e7f7c7a204b47be36
Vincent <57841743+xXatawXx@users.noreply.github.com>
2020-10-13 23:20:06 -0500

    fxmanifest.lua
    correct crediting, for help ofc, couple of white lines, ocd issues ecks dee.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'adamant'
 games { 'rdr3', 'gta5' }
 
-author 'Vinny aka xXatawXx'
-description 'Semi-Advanced Drug System using hashtables {}.'
+author 'Vinny aka xXatawXx ( Thx T1ger )'
+description 'Semi-Advanced Drug System'
 version '1.0.5'
 
 client_scripts {
@@ -11,8 +11,8 @@ client_scripts {
 }
 
 server_scripts {
-	'@async/async.lua',
-	"@mysql-async/lib/MySQL.lua",
+    "@async/async.lua",
+    "@mysql-async/lib/MySQL.lua",
     "config.lua",
     "server/main.lua"
 }
